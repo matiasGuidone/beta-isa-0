8c15b963e6e5d3c6da11cc7890007b55c93b393c
matiasGuidone <matias.guidone@gmail.com>
2020-06-10 18:56:27 -0300

    fix errores

--- Context/AlumnoContext.cs
+++ Context/AlumnoContext.cs
@@ -6,24 +6,28 @@ namespace beta_isa_0.Context
     public class AlumnoContext : DbContext
     {
           public DbSet<Alumno> Alumnos { get; set; }
-          private static string cadena()
-        {
-            XmlTextReader reader = new XmlTextReader("./conexion.xml");
-            while (reader.Read())
-            {
-                if (reader.NodeType == XmlNodeType.Element && reader.Name == "cadena")
-                {
-                    reader.Read();
-                    return reader.Value;
-                }
 
-            }
-            return "";
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseMySql(cadena());
-            base.OnConfiguring(optionsBuilder);
-        }
+          public AlumnoContext(DbContextOptions<AlumnoContext> options) : base(options){
+
+                        }
+        //   private static string cadena()
+        // {
+        //     XmlTextReader reader = new XmlTextReader("./conexion.xml");
+        //     while (reader.Read())
+        //     {
+        //         if (reader.NodeType == XmlNodeType.Element && reader.Name == "cadena")
+        //         {
+        //             reader.Read();
+        //             return reader.Value;
+        //         }
+
+        //     }
+        //     return "";
+        // }
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     optionsBuilder.UseMySql(cadena());
+        //     base.OnConfiguring(optionsBuilder);
+        // }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+ 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+ using beta_isa_0.Context;
 
 namespace beta_isa_0
 {
@@ -23,7 +26,13 @@ namespace beta_isa_0
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+             services.AddDbContext<AlumnoContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection"))
+                );
+             
+
             services.AddControllersWithViews();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
